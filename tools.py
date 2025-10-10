5980d6a9012ba29aeb4b00ae782517a61ee21e1c
Yang Zhao <zepth517@gmail.com>
2025-10-09 21:13:05 -0400

    change the logging to a file

--- tools.py
+++ tools.py
@@ -1,4 +1,11 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[RotatingFileHandler(filename="nest_sync.log",
+                                                  mode="a+",
+                                                  maxBytes=10000000,
+                                                  backupCount=5)]
+                    )
 logger = logging.getLogger(__name__)
